b4ae22b1694ebd121f5d91d6bbe342c493aefb26
Jonathan Wesley Stone <js@piratejon.com>
2013-04-28 09:45:53 -0500

    well that was weird

--- autonome.c
+++ autonome.c
@@ -105,6 +105,7 @@ void count_animals ( Object ** g, int * prabbits, int * pfoxes ) {
 }
 
 void * pick_one_of_three ( void * a, void * b, void * c ) {
+  if ( a == b || b == c || a == c ) *(char*)(0) = 9;
   double r = random_interval();
   if ( r <= 1.0/3.0 ) return a;
   if ( r <= 2.0/3.0 ) return b;
@@ -112,6 +113,7 @@ void * pick_one_of_three ( void * a, void * b, void * c ) {
 }
 
 void * pick_one_of_five ( void * a, void * b, void * c, void * d, void * e ) {
+  if ( a == b || a == c || a == d || a == e || b == c || b == d || b == e || c == d || c == e || d == e ) *(char*)(0) = 9;
   double r = random_interval();
   if ( r <= 1.0/5.0 ) return a;
   if ( r <= 2.0/5.0 ) return b;
@@ -121,6 +123,15 @@ void * pick_one_of_five ( void * a, void * b, void * c, void * d, void * e ) {
 }
 
 void * pick_one_of_eight ( void * a, void * b, void * c, void * d, void * e, void * f, void * g, void * h ) {
+  if ( a == b || a == c || a == d || a == e || a == f || a == g || a == h ||
+                 b == c || b == d || b == e || b == f || b == g || b == h ||
+                           c == d || c == e || c == f || c == g || c == h ||
+                                     d == e || d == f || d == g || d == h ||
+                                               e == f || e == g || e == h ||
+                                                         f == g || f == h ||
+                                                                   g == h )
+    *(char*)(0) = 9;
+
   double r = random_interval();
   if ( r <= 1.0/8.0 ) return a;
   if ( r <= 2.0/8.0 ) return b;
@@ -154,26 +165,65 @@ Object * choose_random_neighbor ( Object ** g, int x, int y ) {
 
   if ( x == 0 ) {
     if ( y == 0 ) {
+      /***
+        x 4
+        6 7
+        ***/
       return pick_one_of_three ( &(g[x+1][y]), &(g[x][y+1]), &(g[x+1][y+1]) );
     } else if ( y + 1 == HEIGHT ) {
+      /***
+        1 2
+        x 4
+        ***/
       return pick_one_of_three ( &(g[x][y-1]), &(g[x+1][y-1]), &(g[x+1][y]) );
     } else {
+      /***
+        1 2
+        x 4
+        6 7
+        ***/
       return pick_one_of_five ( &(g[x][y-1]), &(g[x+1][y-1]), &(g[x+1][y]), &(g[x][y+1]), &(g[x+1][y+1]) );
     }
   } else if ( x + 1 == WIDTH ) {
     if ( y == 0 ) {
+      /***
+        3 x
+        5 6
+        ***/
       return pick_one_of_three ( &(g[x-1][y]), &(g[x-1][y+1]), &(g[x][y+1]) );
     } else if ( y + 1 == HEIGHT ) {
+      /***
+        0 1
+        3 x
+        ***/
       return pick_one_of_three ( &(g[x-1][y-1]), &(g[x][y-1]), &(g[x-1][y]) );
     } else {
+      /***
+        0 1
+        3 x
+        5 6
+        ***/
       return pick_one_of_five ( &(g[x-1][y-1]), &(g[x][y-1]), &(g[x-1][y]), &(g[x-1][y+1]), &(g[x][y+1]) );
     }
   } else {
     if ( y == 0 ) {
+      /***
+        3 x 4
+        5 6 7
+        ***/
       return pick_one_of_five ( &(g[x-1][y]), &(g[x+1][y]), &(g[x-1][y+1]), &(g[x][y+1]), &(g[x+1][y+1]) );
     } else if ( y + 1 == HEIGHT ) {
+      /***
+        0 1 2
+        3 x 4
+        ***/
       return pick_one_of_five ( &(g[x-1][y]), &(g[x-1][y-1]), &(g[x][y-1]), &(g[x+1][y-1]), &(g[x+1][y]) );
     } else {
+      /***
+        0 1 2
+        3 x 4
+        5 6 7
+        ***/
       return pick_one_of_eight ( &(g[x-1][y-1]), &(g[x][y-1]), &(g[x+1][y-1]), &(g[x-1][y]), &(g[x+1][y]), &(g[x-1][y+1]), &(g[x][y+1]), &(g[x+1][y+1]) );
     }
   }
@@ -181,7 +231,7 @@ Object * choose_random_neighbor ( Object ** g, int x, int y ) {
 
 _Bool probabilistic_event_happens ( char * s, double p ) {
   double r = random_interval();
-  printf("Checking probability of event %s: %.2f: %.2f -- %s\n", s, p, r, p >= r ? "Passes":"Fails" );
+  // printf("Checking probability of event %s: %.2f: %.2f -- %s\n", s, p, r, p >= r ? "Passes":"Fails" );
   return p >= r;
 }
 
@@ -189,30 +239,30 @@ void a_neighborly_interaction ( Object * a, Object * n, int x, int y ) {
   if ( a->c == FOX ) {
     if ( n->c == RABBIT ) {
       if ( probabilistic_event_happens ( "Fox eats rabbit", p_f_breed ) ) {
-        printf("(%d,%d): Fox eats rabbit, breeds\n", x, y);
+        // printf("(%d,%d): Fox eats rabbit, breeds\n", x, y);
         n->c = FOX;
         n->age = 0;
       }
     } else {
       if ( probabilistic_event_happens ( "Fox dies", p_f_die ) ) {
-        printf("(%d,%d): Fox dies\n", x, y);
+        // printf("(%d,%d): Fox dies\n", x, y);
         a->c = BARE;
         a->age = 0;
       }
     }
   } else if ( a->c == RABBIT ) {
     if ( n->c == BARE && probabilistic_event_happens ( "Rabbit breeds", p_r_breed ) ) {
-      printf("(%d,%d): Rabbit breeds\n", x, y);
+      // printf("(%d,%d): Rabbit breeds\n", x, y);
       n->c = RABBIT;
       n->age = 0;
     }
   } else {
     if ( n->c != BARE ) {
-      printf("(%d,%d): %s moves\n", x, y, n->c == RABBIT ? "Rabbit" : "Fox");
+      // printf("(%d,%d): %s moves\n", x, y, n->c == RABBIT ? "Rabbit" : "Fox");
       a->c = n->c;
       a->age = n->age;
-      a->c = BARE;
-      a->age = 0;
+      n->c = BARE;
+      n->age = 0;
     }
   }
 }
@@ -322,7 +372,7 @@ int main ( int arfc, char ** arfv ) {
 
   for (i = 0; i < iterations; i+=1 ) {
     iterate(grid, p_r_breed, p_f_breed, p_f_die);
-    sprintf(filename, "%s/%05d.raw", dirname, i);
+    sprintf(filename, "%s/%05d.txt", dirname, i);
     FILE * raw = fopen(filename, "w");
     grid_to_raw(raw, grid);
     fclose(raw);
